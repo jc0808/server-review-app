184cfd0c50b1175a13a815bc68c5acefa9d7b129
Joshua Campos <jdeveloper1203@gmail.com>
2022-10-31 19:34:31 -0400

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   # Add your routes here
   #Games route
-  get "/" do
+  get "/games" do
     games = Game.all
     games.to_json(include: { reviews: { include: :user } })
   end
