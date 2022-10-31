e244661659ccd039297f88490d2d80c113679776
Joshua Campos <jdeveloper1203@gmail.com>
2022-10-31 19:41:40 -0400

    a

--- config/environment.rb
+++ config/environment.rb
@@ -3,36 +3,37 @@
 ENV['RACK_ENV'] ||= "development"
 
 # # Require in Gems
-require 'bundler/setup'
-Bundler.require(:default, ENV['RACK_ENV'])
+# require 'bundler/setup'
+# Bundler.require(:default, ENV['RACK_ENV'])
 
 # # Require in all files in 'app' directory
-require_all 'app'
 
 
 
-# require 'bundler/setup'
-# Bundler.require
-# configure :development do
-#  ENV['RACK_ENV'] ||= "development"
-# require 'bundler/setup'
-#  Bundler.require(:default, ENV['RACK_ENV'])
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "sqlite3",
-#   :database => "db/#{ENV['RACK_ENV']}.sqlite"
+
+require 'bundler/setup'
+Bundler.require
+configure :development do
+ ENV['RACK_ENV'] ||= "development"
+require 'bundler/setup'
+ Bundler.require(:default, ENV['RACK_ENV'])
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :database => "my_database_#{ENV['RACK_ENV']}"
+ )
+ require_all 'app'
+end
+
+# configure :production do
+#  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+#  ActiveRecord::Base.establish_connection(
+#    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#    :host     => db.host,
+#    :username => db.user,
+#    :password => db.password,
+#    :database => db.path[1..-1],
+#    :encoding => 'utf8'
 #  )
 # end
-
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
-end
 
